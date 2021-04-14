f927c7e9cef12dab6cb8d8a61314339c9a33da3e
Rui Conti <ruiconti@gmail.com>
2021-04-14 19:22:20 -0300

    chore: Modifies some helpers

--- kingdom/access/tests/authorization/test_interface.py
+++ kingdom/access/tests/authorization/test_interface.py
@@ -16,7 +16,6 @@ from kingdom.access.authentication.types import (
     encode_user_policies,
 )
 
-
 """
 Problems:
 
@@ -28,12 +27,23 @@ Problems:
 1. We need to have the mininum set of User-Role-Policy domain defined.
 """
 
+JWT = bytes
 
-def default_user() -> User:
+
+def gen_default_user() -> User:
     product_read = Policy(
         resource=Resource("Product"),
         permissions=(Permission.READ,),
-        conditionals=[Conditional("resource.id", "*"), ],
+        conditionals=[
+            Conditional("resource.id", "*"),
+        ],
+    )
+    self_edit = Policy(
+        resource=Resource("User"),
+        permissions=(Permission.UPDATE,),
+        conditionals=[
+            Conditional("resource.id", "ffff"),
+        ],
     )
     specific_coupon = Policy(
         resource=Resource("Coupon"),
@@ -44,14 +54,26 @@ def default_user() -> User:
         ],
     )
     role = Role(
-        name="default consumer", policies=[product_read, specific_coupon]
+        name="default consumer",
+        policies=[self_edit, product_read, specific_coupon],
     )
     user = User(access_key="abc4f", roles=[role])
     return user
 
 
+def gen_invalid_token() -> JWT:
+    from string import ascii_letters
+    from random import randint
+
+    def token_part():
+        return "".join(ascii_letters[randint(0, 50)] for _ in range(1, 64))
+
+    token_str = ".".join(token_part() for _ in range(3))
+    return token_str.encode()
+
+
 def test_successful_encoding_and_decoding():
-    user = default_user()
+    user = gen_default_user()
     user_policies = encode_user_policies(user)
 
     jwt, err_encode = encode_jwt(user)
@@ -64,12 +86,6 @@ def test_successful_encoding_and_decoding():
 
 
 def test_unsuccessful_decoding():
-    from string import ascii_letters
-    from random import randint
-
-    def token_part():
-        return "".join(ascii_letters[randint(0, 50)] for _ in range(1, 64))
-
-    token = ".".join(token_part() for _ in range(3))
-    _, err_decode = decode_jwt(token.encode())
+    invalid_token = gen_invalid_token()
+    _, err_decode = decode_jwt(invalid_token)
     assert isinstance(err_decode, Exception)
